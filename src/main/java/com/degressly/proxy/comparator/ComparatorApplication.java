bd12aeb9334aa43c4cc0ebeec847324eb2fba305
Daniyaal Khan <me@daniyaalkhan.com>
2024-03-24 16:59:28 +0530

    Initial Commit

---
+++ src/main/java/com/degressly/proxy/comparator/ComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.degressly.proxy.comparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/degressly/proxy/comparator/ComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degressly.proxy.comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
